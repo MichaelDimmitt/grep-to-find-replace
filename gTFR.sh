2c6a8642a6e6dd10eacba0c0887655007ba94a2c
MichaelDimmitt <michaelgdimmitt@gmail.com>
2021-03-27 23:50:02 -0400

    set expectations of new functionality to build

--- gTFR.sh
+++ gTFR.sh
@@ -14,6 +14,26 @@ mainLogic() {
   # $2=pattern, what is replace value?
   # $3=filename
 
+  ## Features:
+  # 1) search and replace
+  # 2) search and append to the end of a line
+  # 3) search and append as a new line after the match
+
+  ## Edge cases:
+  # 1) do nothing - tell user why, if file not found
+  # 2) do nothing - tell user why, if multiple occurances are found in file
+  # 3)
+
+  ## Inform the user
+
+  ### Pre-Operation:
+  # 1) grep file for find value, show output of line
+  # 2) grep file for replace value, show output of line
+
+  ### Post-Operation:
+  # 3) grep file for find value, show output of line
+  # 4) grep file for replace value, show output of line
+
 }
 
 sedSearchAndReplace() {
